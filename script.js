c16b3030b73288efb1752b4f432335c8d441d5ca
Akshay Laxmipathi <52963912+Akshay-Laxmipathi@users.noreply.github.com>
2024-09-16 16:51:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,16 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
-  
+	let cost = 0;
+	while(arr.length > 1) {
+	    arr.sort((a,b) => a-b);
+		let first = arr.shift();
+		let second = arr.shift();
+		cost += first + second;
+		arr.push(first + second);
+		arr.sort((a,b) => a-b);
+	}
+	return cost;
 }
 
 module.exports=mincost;
